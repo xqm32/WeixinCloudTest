459210324338e1fcb9b0307908bd779189248e61
xqm32 <458173774@qq.com>
2022-02-13 11:52:12 +0800

    add `TikTokDownload.py`

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -5,6 +5,8 @@ from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
+import TikTokDownload as TK
+
 
 @app.route('/')
 def index():
@@ -64,3 +66,11 @@ def get_count():
     """
     counter = Counters.query.filter(Counters.id == 1).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+@app.route('/api/tiktok', methods=['GET'])
+def tiktok():
+    """
+    :return: TikTok 视频下载测试
+    """
+    TK.video_download('https://v.douyin.com/LG7nvHP/', 'no')
+    return 'Downloaded Successfully'
